43be39fc537b6b77920ca703fb559800c6994cbd
Mladen <blagmash@gmail.com>
2020-04-06 16:02:04 +0200

    home page added  to router

---
+++ src/app/index.ts
@@ -0,0 +1,7 @@
+export * from './app-routing.module';
+export * from './app.component';
+export * from './app.module';
+export * from './home';
+export * from './app/navbar';
+export * from './app/layout/layout.component';
+export * from './app/navbar/core/nabar-item';
