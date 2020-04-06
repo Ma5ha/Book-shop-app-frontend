c021400dcfa65c7a5b415ab43ee09786a6184387
Mladen <blagmash@gmail.com>
2020-04-06 15:19:09 +0200

    layaout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './app/navbar/navbar.component';
+import { LayoutComponent } from './app/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
