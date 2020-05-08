e407d805e23d3b26e14f5edfe2754b7407e2fcc3
Mladen <blagmash@gmail.com>
2020-05-08 16:28:24 +0200

    carts removed but has many is there

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+    has_many :cart
+    has_many :user, through: :cart
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,13 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_115407) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "book_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
