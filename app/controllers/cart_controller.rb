45c87fde9260b428e8c9e5d7fc68df64c45e8002
Mladen <blagmash@gmail.com>
2020-05-10 10:40:43 +0200

    cart is working

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,16 @@
 class CartController < ApplicationController
+    def index
+        
+    end
+    def create
+        
+       Cart.create(book_id:params[:book_id],user_id:params[:user_id])
+    end
+    
+    def show
+        @user =User.find(params[:id])
+        @book= @user.books
+        render json: @book
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :books
+  resources :cart
   get 'book', action: :show, controller: 'books'
   
 get 'profile', action: :show, controller: 'users'
