ec04f8f9def21daa8c0124496877df97425f4fa2
Mladen <blagmash@gmail.com>
2020-06-09 10:45:28 +0200

    to top scroll

--- src/app/app/layout/layout.component.ts
+++ src/app/app/layout/layout.component.ts
@@ -11,5 +11,10 @@ export class LayoutComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onActivate(event) {
+    window.scroll(0, 0);
+    //or document.body.scrollTop = 0;
+    //or document.querySelector('body').scrollTo(0,0)
 
+  }
 }
